67881d2a055deb8dd509ab20dae7145680db8b82
Tata <143838354+thaemegregorio@users.noreply.github.com>
2024-09-17 16:05:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é refatoração de código e por que ela é importante??',
+    'Código bem estruturado é mais fácil de ler e entender, o que facilita a colaboração entre desenvolvedores e a manutenção a longo prazo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'Como você explicaria o conceito de "closure" em JavaScript??',
+    'O conceito de "closure" (ou "fechamento") em JavaScript é um aspecto fundamental da linguagem que permite que funções preservem o acesso às variáveis de seu escopo externo, mesmo depois que esse escopo tenha sido encerrado. '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bonita em Inglês?',
+    'Bonita em ingles é beutiful(beutiful)'
+)
